45d49cf1c98284a768bd3edc51bd11edc2025ec0
yo.ya <yo.ya@example.com>
2018-07-30 11:24:21 +0000

    add have ranking

--- app/controllers/rankings_controller.rb
+++ app/controllers/rankings_controller.rb
@@ -3,4 +3,9 @@ class RankingsController < ApplicationController
     @ranking_counts = Want.ranking
     @items = Item.find(@ranking_counts.keys)
   end
+  
+  def have
+    @have_ranking_counts = Have.ranking
+    @items = Item.find(@have_ranking_counts.keys)
+  end
 end
